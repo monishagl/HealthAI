dfe6155a0c96187464f179f6bbba6274c0514889
monishagl <154099505+monishagl@users.noreply.github.com>
2024-10-07 18:01:14 -0700

    Initial commit

---
+++ HealthAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthcare.HealthAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
